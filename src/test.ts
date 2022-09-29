485d9026f5fa58202f5ae532d376d840c485cbdc
Rasmus Schultz <rasmus@mindplay.dk>
2022-09-29 16:43:13 +0200

    strip it down to a console.log

--- src/test.ts
+++ src/test.ts
@@ -1,17 +1,14 @@
-import { setup, assertions, run, statusOf, printReport } from "inertion";
+import { setup, assertions, run } from "inertion";
+import * as util from "util";
 
 const test = setup(assertions);
 
 test(`yo`, async is => {
-  console.log(new Error("oh noes"));
-
   is.ok(false, "oh noes");
 });
 
 (async () => {
   const results = await run(test);
 
-  printReport(results);
-
-  process.exit(statusOf(results));
+  console.log(util.inspect(results, { depth: 99 }));
 })();
